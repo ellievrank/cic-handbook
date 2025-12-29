43c8c21c873e8b76bfed63fa4dfbb64cf7e90b8a
Ellie Vrankovich <ellievrank@gmail.com>
2025-12-29 13:59:37 +0800

    Made footer, added support services page

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,6 +28,7 @@ export default defineConfig({
             ],
             components: {
                 Header: './src/components/Header.astro',
+                Footer: './src/components/Footer.astro',
             },
         }),
     ],
