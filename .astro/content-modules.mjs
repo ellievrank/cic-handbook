45418cfb2e6af808c30159fd6fea8924099a49c2
Ellie Vrankovich <ellievrank@gmail.com>
2026-01-02 16:04:06 +0800

    Add acknowledgement page and update footer

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,6 +1,7 @@
 
 export default new Map([
 ["src/content/docs/support-services/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fsupport-services%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/wa-guide/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fwa-guide%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")]]);
+["src/content/docs/acknowledgements/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Facknowledgements%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/wa-guide/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fwa-guide%2Findex.mdx&astroContentModuleFlag=true")]]);
 		
